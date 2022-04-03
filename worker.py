27d0a69c8eec01a8927702e91aa03a0ba4d6a8b6
danielromeroy <s220260@student.dtu.dk>
2022-04-03 20:15:40 +0200

    Finished and commented

--- worker.py
+++ worker.py
@@ -1,20 +1,16 @@
+# Worker script for exercise 1
+
 import sys
-import time
+import os
 
 # Receive paths from args
 INFILE_PATH = sys.argv[1]
 OUTFILE_PATH = sys.argv[2]
 
-out_seq = []
-
 infile = open(INFILE_PATH, "rb")
-for line in infile:
-    if line[0].to_bytes(1, "little") == b">":
-        out_seq_header = line  # Save header line
-    else:
-        out_seq.append(line)  # Add sequence lines to list
-
-out_seq = b"".join(out_seq)  # Join list
+size = os.path.getsize(INFILE_PATH)
+out_seq = infile.read(size)
+out_seq = out_seq[out_seq.find(b"\n"):]
 
 # Use translation table for complementary
 translationTable = bytes.maketrans(b"atcg", b"tagc")
@@ -23,18 +19,17 @@ out_seq = out_seq.translate(translationTable)
 out_seq = out_seq[::-1]  # Reverse
 
 # Format header with sequence counts
-out_seq_header = b">seq" + bytes(f"{sys.argv[1]}", "utf-8") + \
+out_seq_header = b">seq" + bytes(f"{sys.argv[3]}", "utf-8") + \
                  b" A: " + bytes(str(out_seq.count(b"a")), "utf-8") + \
                  b" C: " + bytes(str(out_seq.count(b"c")), "utf-8") + \
                  b" T: " + bytes(str(out_seq.count(b"t")), "utf-8") + \
                  b" G: " + bytes(str(out_seq.count(b"g")), "utf-8") + \
-                 b" N: " + bytes(str(out_seq.count(b"n")), "utf-8")
+                 b" N: " + bytes(str(out_seq.count(b"n")), "utf-8") + b"\n"
 
 infile.close()
 
 # Write out file
 outfile = open(OUTFILE_PATH, "wb")
 outfile.write(out_seq_header)
-outfile.write(b"\n")
 outfile.write(out_seq)
 outfile.close()
